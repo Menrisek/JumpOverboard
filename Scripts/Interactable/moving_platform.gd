80c99fb5a56b62ef25df79fd9f792ad2ee234f06
Šimon Přikryl <s.prikryl06@seznam.cz>
2025-02-24 16:08:56 +0100

    Icony srdcí, Boost itemů

--- Scripts/Interactable/moving_platform.gd
+++ Scripts/Interactable/moving_platform.gd
@@ -4,8 +4,13 @@ class_name MovingPlatform
 @onready var animation = $AnimatableBody2D/AnimationPlayer
 
 @export var timer = 0.0
+@export var moving_up = false
 
 func _ready():
 	#zde je v animaci i ten pohyb
-	await get_tree().create_timer(timer).timeout
-	animation.play("move")
+	if moving_up == true:
+		await get_tree().create_timer(timer).timeout
+		animation.play("move_up")
+	else:
+		await get_tree().create_timer(timer).timeout
+		animation.play("move")
